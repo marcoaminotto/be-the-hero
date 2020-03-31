39d1716cb1d0ebc13a388bbaea4ec052217c30af
Marco Echevestre <marcoaminotto@hotmail.com>
2020-04-01 00:27:44 +0200

    Update a style at Login page, build the Profile page

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewIncident(params) {
+  return <h1>teste</h1>;
+}
+
+export default NewIncident;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 function Routes(params) {
   return (
@@ -10,6 +12,8 @@ function Routes(params) {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
